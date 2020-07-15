e75938a0b1eef6eb565971093c80b4bbf9eddd90
Berkant BEGDILILI <59812010+berkantbegdilili@users.noreply.github.com>
2020-07-15 19:18:34 +0300

    Initial Commit

---
+++ DPURingV2/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  DPURingV2
+//
+//  Created by Berkant Beğdilili on 15.07.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
